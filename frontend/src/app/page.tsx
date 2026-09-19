8ef03a66ac0b5cf9d4e62dadafe72e9a2647b578
Farati <adefaratibusolami@gmail.com>
2026-09-19 16:42:34 +0100

    Fix page background to show gradient

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import DebugContracts from '../components/debug/DebugContracts';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#131416] text-white">
+    <main className="min-h-screen text-white">
       <div className="max-w-4xl mx-auto px-4">
         <About />
         <DebugContracts />
